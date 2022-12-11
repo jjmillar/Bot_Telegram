79e90678bff3c5adf21f541f7eff211a55b94aeb
Julián Millar <julianjmillar@gmail.com>
2022-12-11 10:20:46 -0300

    Add files via upload
    Railway deploy

--- index.js
+++ index.js
@@ -1,57 +1,111 @@
-// const { Telegraf } = require('telegraf');
+// const { Telegraf } = require('telegraf'); // Para versión sin microbot
 const { Composer } = require('micro-bot');
 
-// const bot = new Telegraf('5537954512:AAGpZub10GXYaESwkpv0h2stL5bWp3lQL5A');
+// const bot = new Telegraf('BOT_TOKEN'); // Para versión sin microbot
 const bot = new Composer();
 
+/**
+ *  COMANDOS BASICOS
+ */
 
-bot.start((ctx) => {
-        ctx.reply('Hola' + ' ' + ctx.from.first_name);     
-});
+bot.start((ctx) => {ctx.reply('Hola ' + ctx.from.first_name);});
+bot.help((ctx) => {ctx.reply('Escribe /helio o /bot');});
+bot.on('new_chat_members', (ctx) => {ctx.reply('Bienvenido ' + ctx.from.first_name + ' al grupo de telegram de Muramasa BJJ. Respeto ante todo y usemos el grupo para hacer crecer nuestro Jiujitsu\n' + 'Si necesitas mas información, solo escribe /helio y te ayudaré en lo que pueda.');});
+bot.hears(['❤️'], ctx => {ctx.reply('❤️');});
+bot.hears(['estas helio?'], ctx => {ctx.reply('Si, aquí estoy ' + ctx.from.first_name);});
+bot.hears(['Wena helio','Wena Helio', 'wena helio'], ctx => {ctx.reply('Wena wena' + ctx.from.first_name);});
+bot.on('left_chat_member', (ctx) => {ctx.reply('Farewell ' + ctx.from.first_name);});
 
-bot.help((ctx) => {
-        ctx.reply('Escribe /helio o /bot');    
-});
-
-bot.on('new_chat_members', (ctx) => {
-        ctx.reply('Bienvenido ' + ctx.from.first_name + ' al grupo de telegram de Muramasa BJJ. Respeto ante todo y usemos el grupo para hacer crecer nuestro Jiujitsu\n' + 'Si necesitas mas información, solo escribe mi nombre y te ayudaré en lo que pueda.');
-});
-
-// bot.url((ctx) => {
-//         ctx.reply('Buen video ' + ctx.from.first_name);
-// });
-
-bot.hears(['Gracias helio'], ctx => {
-        ctx.reply('❤️');
-});
-
-bot.hears(['Despertaste helio', 'despertaste helio'], ctx => {
-    ctx.reply('Ahora trabajo 24/7');
-});
-
-bot.hears(['Wena helio','Wena Helio', 'wena helio'], ctx => {
-    ctx.reply('Wena wena' + ctx.from.first_name);
-});
+/**
+ *  INLINE MENU COMPLETO
+ */
 
 //MENU PRINCIPAL
 
-bot.command(["helio","bot","Helio", "Oie Helio"], (ctx) => {
+const menu = (ctx) => {
     ctx.deleteMessage()
     ctx.reply("Hola " + ctx.from.first_name + ' ' + '.Pincha los botones de abajo para ayudarte en lo que pueda.\n' + 'Para no spamear el chat, favor de hacer click en el boton SALIR cuando ya no me necesites mas.\n', {
         reply_markup: {
             inline_keyboard: [
                 [ { text: "Horarios", callback_data: "btn-horarios" }, { text: "Eventos", callback_data: "btn-eventos" } ],
-                [ { text: "Facebook", url: "https://www.facebook.com/muramasabjj" }, { text: "Instagram", url: "https://www.instagram.com/muramasa_bjj" } ],
+                [ { text: "Redes Sociales", callback_data: "btn-rrss" } ],
                 [ { text: "Playlist Spotify", url:"https://open.spotify.com/playlist/4PQ5KrbS64CaRjFXdzeJDP?si=b3af847cfe5143d0" } ],
+                [ { text: "Material de Lectura", callback_data: "btn-material" } ],
                 [ { text: "Salir", callback_data: "btn-salir" } ],
             ]
         }
     })
-});
+};
 
-//BOTÓN HORARIOS
+//MATERIAL
 
-bot.action('btn-horarios', (ctx) => {
+const material = (ctx) => {
+    ctx.deleteMessage() 
+    ctx.reply('Elige el tipo de material que deseas leer.\nYa iremos agregando material de a poco 💪🏻🥋🤘🏻', {
+        reply_markup: {
+            inline_keyboard: [
+                [ { text: "Reglamentos", callback_data: "btn-reglamentos" } ],
+                [ { text: "Apuntes", callback_data: "btn-apuntes" } ],
+                [ { text: "Atrás", callback_data: "btn-back-menu" } ],
+                [ { text: "Salir", callback_data: "btn-salir" } ],
+            ]
+        }
+    })
+};
+
+//APUNTES
+
+const apuntes = (ctx) => {
+    ctx.deleteMessage() 
+    ctx.reply('Elige el apunte que desees leer.', {
+        reply_markup: {
+            inline_keyboard: [
+                [ { text: "Diccionario Japones-Español", url: "https://drive.google.com/file/d/1-D6b7pBpJu3TOHBks_t4bXhFI247h2E3/view?usp=sharings" } ],
+                [ { text: "Atrás", callback_data: "btn-material" } ],
+                [ { text: "Salir", callback_data: "btn-salir" } ],
+            ]
+        }
+    })
+};
+
+//REDES SOCIALES
+
+const rrss = (ctx) => {
+    ctx.deleteMessage() 
+    ctx.reply('Acá tienes nuestras redes sociales 🤓', {
+        reply_markup: {
+            inline_keyboard: [
+                [ { text: "Facebook", url: "https://www.facebook.com/muramasabjj" } ],
+                [ { text: "Instagram", url: "https://www.instagram.com/muramasa_bjj" } ],
+                [ { text: "Muramasa TV", url: "https://t.me/+6nCjXUlzsM1iZjg5" } ],
+                [ { text: "Atrás", callback_data: "btn-back-menu" } ],
+                [ { text: "Salir", callback_data: "btn-salir" } ],
+            ]
+        }
+    })
+};
+
+//REGLAMENTOS
+
+const reglamentos = (ctx) => {
+    ctx.deleteMessage() 
+    ctx.reply('Elige el reglamento que quieras revisar. El de ADCC está en español, pero el de IBJJF está en inglés. Pronto traduciré el reglamento 🤓', {
+        reply_markup: {
+            inline_keyboard: [
+                [ { text: "ADCC", url: "https://drive.google.com/file/d/1j-eD7vhFPwUZZpBzfI2VEzdKKnfBDsgQ/view?usp=sharing" } ],
+                [ { text: "IBJJF Libro de reglas", url: "https://drive.google.com/file/d/1sax9rYff5lj-KHo5c4iEK17O7c78ycQG/view?usp=sharing" } ],
+                [ { text: "IBJJF Guia de actualización de reglas", url: "https://drive.google.com/file/d/1hoDZ2TwdUoMQ9ffP_nHzc_LFyXQ0aTib/view?usp=sharing" } ],
+                [ { text: "IBJJF Poster de movimientos", url: "https://drive.google.com/file/d/1TflN5JqrukrAG1nFtsfQjX9wkfYcqJWJ/view?usp=sharing" } ],
+                [ { text: "Atrás", callback_data: "btn-material" } ],
+                [ { text: "Salir", callback_data: "btn-salir" } ],
+            ]
+        }
+    })
+};
+
+//HORARIOS
+
+const horarios = (ctx) => {
     ctx.deleteMessage() 
     ctx.reply('Lunes: 21:00-23:00 / Gi / Clase de Fundamentales del BJJ\n' + 'Martes: 20:00-22:00 / NoGi / Clase de BJJ y Grappling\n' + 'Jueves: 20:00-22:00 / Gi / Clase avanzada', {
         reply_markup: {
@@ -61,13 +115,13 @@ bot.action('btn-horarios', (ctx) => {
             ]
         }
     })
-});
+};
 
-//BOTÓN EVENTOS
+//EVENTOS
 
-bot.action('btn-eventos', (ctx) => {
+const eventos = (ctx) => {
     ctx.deleteMessage() 
-    ctx.reply('27 de Agosto torneo de Grappling en ED HQ, reglas ADCC. La info está fijada en el chat!!.\n', {
+    ctx.reply('17 de Diciembre. Aniversario Elemetal Dojo en Temuco!\n A ponerle el hombro al treino ya que habrá mucha gente y sorpresas!\n A programarse desde ya!', {
         reply_markup: {
             inline_keyboard: [
                 [ { text: "Atrás", callback_data: "btn-back-menu" } ],
@@ -75,37 +129,22 @@ bot.action('btn-eventos', (ctx) => {
             ]
         }
     })
-});
+};
+
+//COMANDOS BOTONES
+
+bot.command(["helio","bot","Helio", "Oie Helio"], menu);
+bot.action("btn-back-menu", menu);
+bot.action('btn-material', material);
+bot.action('btn-apuntes', apuntes);
+bot.action('btn-rrss', rrss);
+bot.action('btn-reglamentos', reglamentos);
+bot.action('btn-horarios', horarios);
+bot.action('btn-eventos', eventos);
+bot.action('btn-salir', (ctx) => {ctx.deleteMessage()});
 
 
-//BOTÓN BACK A MENÚ
-
-bot.action("btn-back-menu", (ctx) => {
-    ctx.deleteMessage()
-    ctx.reply("Hola " + ctx.from.first_name + ' ' + '.Pincha los botones de abajo para ayudarte en lo que pueda.\n' + 'Para no spamear el chat, favor de hacer click en el boton SALIR cuando ya no me necesites mas.\n', {
-        reply_markup: {
-            inline_keyboard: [
-                [ { text: "Horarios", callback_data: "btn-horarios" }, { text: "Eventos", callback_data: "btn-eventos" } ],
-                [ { text: "Facebook", url: "https://www.facebook.com/muramasabjj" }, { text: "Instagram", url: "https://www.instagram.com/muramasa_bjj" } ],
-                [ { text: "Playlist Spotify", url:"https://open.spotify.com/playlist/4PQ5KrbS64CaRjFXdzeJDP?si=b3af847cfe5143d0" } ],
-                [ { text: "Salir", callback_data: "btn-salir" } ],
-            ]
-        }
-    })
-});
-
-//BOTÓN SALIR
-bot.action('btn-salir', (ctx) => {
-    ctx.deleteMessage()
-    // ctx.reply('❤️')
-    // setTimeout(() => {console.log("1 Segundo esperado");}, 2000);
-});
-
-bot.hears(['/helio', '/bot', 'helio', '/helio@HelioGBot', '/bot@HelioGBot'], ctx => {
-    ctx.deleteMessage();
-});
-
-// bot.launch();
+// bot.launch(); // Para versión sin microbot
 module.exports = bot;
 
 //afternoon-oasis-13635
